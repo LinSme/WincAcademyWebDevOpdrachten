d3c88f25113fe03ac25ba4f33266a41fc02f9354
LinSme <87670030+LinSme@users.noreply.github.com>
2021-08-24 12:13:47 +0200

    Add files via upload

---
+++ 12. Opdracht - Gebruik je eerste JavaScript/index.js
@@ -0,0 +1,25 @@
+console.log("Hello Winc Academy");
+
+let myName = "My name is Linda";
+console.log(myName);
+
+//let if = Dit kan niet, want if is al een gebruikte terminologie in de taal
+
+let sumNumbers = 2 + 2;
+console.log(sumNumbers);
+
+let sumString = "2+2";
+console.log(sumString);
+//Je kunt niet optellen met teksten, alleen met nummers (zoals in excel ook bijvoorbeeld)
+
+let uitgebreideSom = 1 + 2 - 4 * 5 / 6;
+console.log(uitgebreideSom);
+
+let modulus = 9 % 4;
+console.log(modulus);
+// Heb wel even moeten googlen wat een modulus doet -> je deelt hier 9 door 4. Iedereen krijgt dan 2 stukken, maar er blijft er 1 over. De modulus is dan 1. 
+
+let age = 37;
+console.log(typeof (age));
+age = "Zevenendertig";
+console.log(typeof (age));
