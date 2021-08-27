3a86bf3a3393cce8043b06025f10161262df59c5
LinSme <87670030+LinSme@users.noreply.github.com>
2021-08-27 15:52:47 +0200

    Add files via upload

---
+++ 14. Opdracht - Gebruik if else statements/script.js
@@ -0,0 +1,42 @@
+console.log("") //Zo ziet de uitkomst er in de Terminal wat rustiger uit
+console.log("Opdracht 14. If/else statements")
+console.log("") //Zo ziet de uitkomst er in de Terminal wat rustiger uit
+
+//Opdracht deel 1
+console.log("deel 1: leeftijdscheck")
+
+const age = 18;
+
+if (age >= 18) {
+    console.log("The age 18 or above; person can enter the bar")
+} else {
+    console.log("The age is below 18; please go away")
+}
+
+console.log("")//Zo ziet de uitkomst er in de Terminal wat rustiger uit
+
+//Opdracht deel 2
+console.log("deel 2: ladiesnight")
+
+const isFemale = false;
+
+if (isFemale) {
+    console.log("The person is female and can enter on ladiesnight")
+} else {
+    console.log("The person is male and cannot enter on ladiesnight")
+}
+
+console.log("")//Zo ziet de uitkomst er in de Terminal wat rustiger uit
+
+//Opdracht deel 3
+console.log("deel 3: When you drive, never drink")
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+    console.log("The person is fit to drive")
+} else {
+    console.log("The person not fit to drive")
+}
+
+console.log("")//Zo ziet de uitkomst er in de Terminal wat rustiger uit
