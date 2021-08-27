dfc5c5c2c4004852dee2ba89127919de8121fad3
LinSme <87670030+LinSme@users.noreply.github.com>
2021-08-27 15:06:18 +0200

    Add files via upload

---
+++ 13. Opdracht - JS programma draaien op jouw computer + comments maken/script.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy studenten")
+
+/* Dit is een blok comment; hierdoor wordt de gehele code nu niet langer gerund
+    //Dit is een grote som; deze tekst zul je niet zien omdat het een comment is, en comments niet gerund worden.
+    let number1 = 1230941
+    let number2 = 1823794
+    console.log(number1 * number2)
+    //of directer kan ook:
+    console.log(1230941 * 1823794)
+    //of nog uitgebreidere defined variables kan ook
+    let uitkomst1 = number1 * number2
+    console.log(uitkomst1)
+
+    console.log(637263/54)
+*/
+console.log("klaar")
