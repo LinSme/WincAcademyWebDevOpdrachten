0723044b18ba15608dbbece1bde55de9080e0382
LinSme <87670030+LinSme@users.noreply.github.com>
2021-08-28 20:59:55 +0200

    Add files via upload

---
+++ 17. Opdracht Function Basics/script.js
@@ -0,0 +1,24 @@
+
+makePizza = function () {
+    console.log("")
+    console.log("Maak en simpele margarita:");
+    console.log("Pak pizzabodem");
+    console.log("Smeer tomatensaus op pizzabodem");
+    console.log("Strooi er kaas overheen");
+    console.log("")
+}
+
+makeSushi = function () {
+    console.log("")
+    console.log("Maak de sushi:")
+    console.log("Neem een lepel sushirijst");
+    console.log("Leg er een stukje zalm op");
+    console.log("");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
