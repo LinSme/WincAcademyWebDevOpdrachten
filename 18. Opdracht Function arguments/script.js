de1857baaaae98abbd24039de5d4605f26191525
LinSme <87670030+LinSme@users.noreply.github.com>
2021-08-28 21:31:21 +0200

    Add files via upload

---
+++ 18. Opdracht Function arguments/script.js
@@ -0,0 +1,10 @@
+paintWall = function (whichWall, color) {
+    console.log("");
+    console.log("The " + whichWall + " wall has been painted " + color);
+    console.log("");
+};
+
+let color = 'orange';
+let whichWall = 'North';
+
+paintWall(whichWall, color);
